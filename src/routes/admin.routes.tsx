1b580f66c015aa047d4333e2697ee43165e3bc09
Md Shihab Uddin <uddinmdshihab452@gmail.com>
2024-01-16 19:31:14 +0600

    5-8 Refactor Hard Coded Admin Routes

--- src/routes/admin.routes.tsx
+++ src/routes/admin.routes.tsx
@@ -2,26 +2,79 @@ import AdminDashboard from "../pages/admin/AdminDashboard";
 import CreateStudent from "../pages/admin/CreateStudent";
 import CreateAdmin from "../pages/admin/CreateAdmin";
 import CreateFaculty from "../pages/admin/CreateFaculty";
+import { ReactNode } from "react";
+type TPath = {
+  path: string;
+  element: ReactNode;
+};
 
-export const adminPaths = [
+const path = [
   {
-    index: true, //if we want to show admin dashboard on /admin path then
-    element: <AdminDashboard />,
-  },
-  {
+    name: "Dashboard",
     path: "dashboard",
     element: <AdminDashboard />,
   },
   {
-    path: "create-student",
-    element: <CreateStudent />,
-  },
-  {
-    path: "create-admin",
-    element: <CreateAdmin />,
-  },
-  {
-    path: "create-faculty",
-    element: <CreateFaculty />,
+    name: "User Management",
+    children: [
+      {
+        name: "Create Admin",
+        path: "create-admin",
+        element: <CreateAdmin />,
+      },
+      {
+        name: "Create Faculty",
+        path: "create-faculty",
+        element: <CreateFaculty />,
+      },
+      {
+        name: "Create Student",
+        path: "create-student",
+        element: <CreateStudent />,
+      },
+    ],
   },
 ];
+
+//* Functional way for create dynamic route
+export const adminPaths = path.reduce((acc: TPath[], item) => {
+  if (item.path && item.element) {
+    acc.push({
+      path: item.path,
+      element: item.element,
+    });
+  }
+  if (item.children) {
+    item.children.forEach((child) =>
+      acc.push({
+        path: child.path,
+        element: child.element,
+      })
+    );
+  }
+  return acc;
+}, []);
+
+//! Hard coded value for routes
+// export const adminPaths = [
+//   {
+//     index: true, //if we want to show admin dashboard on /admin path then
+//     element: <AdminDashboard />,
+//   },
+//   {
+//     path: "dashboard",
+//     element: <AdminDashboard />,
+//   },
+//   {
+//     path: "create-student",
+//     element: <CreateStudent />,
+//   },
+//   {
+//     path: "create-admin",
+//     element: <CreateAdmin />,
+//   },
+//   {
+//     path: "create-faculty",
+//     element: <CreateFaculty />,
+//   },
+// ];
